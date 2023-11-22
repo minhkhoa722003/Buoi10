93215ed1a3a85e36fd6eb265633db77b133e18c4
NguyenMinhKhoa <mjnhkhoa722003@gmail.com>
2023-11-22 16:04:48 +0700

    Buoi 10

---
+++ WindowsFormsApp/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.radioButtonA = new System.Windows.Forms.RadioButton();
+            this.radioButtonB = new System.Windows.Forms.RadioButton();
+            this.radioButtonC = new System.Windows.Forms.RadioButton();
+            this.radioButtonD = new System.Windows.Forms.RadioButton();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(23, 28);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(35, 13);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "label1";
+            // 
+            // radioButtonA
+            // 
+            this.radioButtonA.AutoSize = true;
+            this.radioButtonA.Location = new System.Drawing.Point(26, 62);
+            this.radioButtonA.Name = "radioButtonA";
+            this.radioButtonA.Size = new System.Drawing.Size(32, 17);
+            this.radioButtonA.TabIndex = 1;
+            this.radioButtonA.TabStop = true;
+            this.radioButtonA.Text = "A";
+            this.radioButtonA.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonB
+            // 
+            this.radioButtonB.AutoSize = true;
+            this.radioButtonB.Location = new System.Drawing.Point(26, 100);
+            this.radioButtonB.Name = "radioButtonB";
+            this.radioButtonB.Size = new System.Drawing.Size(32, 17);
+            this.radioButtonB.TabIndex = 2;
+            this.radioButtonB.TabStop = true;
+            this.radioButtonB.Text = "B";
+            this.radioButtonB.UseVisualStyleBackColor = true;
+            this.radioButtonB.CheckedChanged += new System.EventHandler(this.radioButtonB_CheckedChanged);
+            // 
+            // radioButtonC
+            // 
+            this.radioButtonC.AutoSize = true;
+            this.radioButtonC.Location = new System.Drawing.Point(26, 134);
+            this.radioButtonC.Name = "radioButtonC";
+            this.radioButtonC.Size = new System.Drawing.Size(32, 17);
+            this.radioButtonC.TabIndex = 3;
+            this.radioButtonC.TabStop = true;
+            this.radioButtonC.Text = "C";
+            this.radioButtonC.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonD
+            // 
+            this.radioButtonD.AutoSize = true;
+            this.radioButtonD.Location = new System.Drawing.Point(25, 169);
+            this.radioButtonD.Name = "radioButtonD";
+            this.radioButtonD.Size = new System.Drawing.Size(33, 17);
+            this.radioButtonD.TabIndex = 4;
+            this.radioButtonD.TabStop = true;
+            this.radioButtonD.Text = "D";
+            this.radioButtonD.UseVisualStyleBackColor = true;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(157, 118);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 13);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.Click += new System.EventHandler(this.labelResult_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.radioButtonD);
+            this.Controls.Add(this.radioButtonC);
+            this.Controls.Add(this.radioButtonB);
+            this.Controls.Add(this.radioButtonA);
+            this.Controls.Add(this.labelQuestion);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.RadioButton radioButtonA;
+        private System.Windows.Forms.RadioButton radioButtonB;
+        private System.Windows.Forms.RadioButton radioButtonC;
+        private System.Windows.Forms.RadioButton radioButtonD;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ WindowsFormsApp/MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace WindowsFormsApp
+{
+    public partial class MainForm : Form
+    {
+        private int currentQuestionIndex = 0;
+        private string[] questions = { "Câu hỏi 1: Đâu là quốc gia lớn nhất thế giới?", "Câu hỏi 2: Ai là người đầu tiên đặt chân lên mặt trăng?","Câu 3: Bạn nghĩ kì này bạn rớt môn gì ?" };
+        private string[][] answers = { new string[] { "A. Nga", "B. Mỹ", "C. Trung Quốc", "D. Ấn Độ" }, new string[] { "A. Neil Armstrong", "B. Buzz Aldrin", "C. Yuri Gagarin", "D. Alan Shepard" }, new string[] {"A. Lập trình web","B. Công nghệ NET","C. Hệ quản trị CSDL","Trí tuệ nhân tạo"} };
+        private string[] correctAnswers = { "B", "A","A" };
+        private string[] selectedAnswers;
+        private string[] userAnswers = new string[2];
+        public MainForm()
+        {
+            InitializeComponent();
+            selectedAnswers = new string[questions.Length];
+            DisplayQuestion();
+        }
+
+        private void DisplayQuestion()
+        {
+            if (currentQuestionIndex >= 0 && currentQuestionIndex < questions.Length)
+            {
+                labelQuestion.Text = questions[currentQuestionIndex];
+                radioButtonA.Text = answers[currentQuestionIndex][0];
+                radioButtonB.Text = answers[currentQuestionIndex][1];
+                radioButtonC.Text = answers[currentQuestionIndex][2];
+                radioButtonD.Text = answers[currentQuestionIndex][3];
+                if (selectedAnswers[currentQuestionIndex] == "A")
+                    radioButtonA.Checked = true;
+                else if (selectedAnswers[currentQuestionIndex] == "B")
+                    radioButtonB.Checked = true;
+                else if (selectedAnswers[currentQuestionIndex] == "C")
+                    radioButtonC.Checked = true;
+                else if (selectedAnswers[currentQuestionIndex] == "D")
+                    radioButtonD.Checked = true;
+                else
+                {
+                    radioButtonA.Checked = false;
+                    radioButtonB.Checked = false;
+                    radioButtonC.Checked = false;
+                    radioButtonD.Checked = false;
+                }
+
+                button1.Enabled = currentQuestionIndex > 0;
+                button2.Enabled = currentQuestionIndex < questions.Length - 1;
+            }
+        }
+        private void radioButtonA_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonA.Checked)
+                selectedAnswers[currentQuestionIndex] = "A";
+        }
+
+        private void radioButtonB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonB.Checked)
+                selectedAnswers[currentQuestionIndex] = "B";
+        }
+
+        private void radioButtonC_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonC.Checked)
+                selectedAnswers[currentQuestionIndex] = "C";
+        }
+
+        private void radioButtonD_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonD.Checked)
+                selectedAnswers[currentQuestionIndex] = "D";
+        }
+
+
+
+        private void labelResult_Click(object sender, EventArgs e)
+        {
+            if (selectedAnswers[currentQuestionIndex] == "B")
+            {
+                labelResult.Text = "Kết quả: Đúng";
+            }
+            else
+            {
+                labelResult.Text = "Kết quả: Sai";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentQuestionIndex--;
+            DisplayQuestion();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentQuestionIndex++;
+            DisplayQuestion();
+        }
+    }
+}
